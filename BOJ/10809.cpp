39d97dc9db91e3233bc7b9b6265c36ba902a97ef
Junhyeok Yun <claudead777@gmail.com>
2023-03-31 21:32:12 +0900

    2023/03/31

---
+++ BOJ/10809.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int a[26];
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    fill(a, a+26, -1);
+    string s;
+    cin >> s;
+    for (int i = 0; i < s.length(); i++) {
+        if (a[s[i] - 'a'] == -1) a[s[i] - 'a'] = i;
+    }
+    for (int i = 0; i < 26; i++) cout << a[i] << ' ';
+}
